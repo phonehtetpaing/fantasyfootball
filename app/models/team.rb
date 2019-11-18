b1065502ce1a7a90114e1229c81062b4704f13ba
Stephan <phonehtetpaing1221@gmail.com>
2019-11-18 10:26:59 +0630

    minor  changes

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,6 +5,8 @@ class Team < ApplicationRecord
   before_save :cal_scores
 
   def cal_scores
-    self.score = (self.wins.to_f/self.total_matches.to_f)*100
+    if self.wins != 0
+      self.score = (self.wins.to_f/self.total_matches.to_f)*100
+    end
   end
 end
