8d10dd8244297f413b655cdf37c3e7f52c52577f
Stephan <phonehtetpaing1221@gmail.com>
2019-11-18 10:40:47 +0630

    minor  changes

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,5 +2,5 @@ class Match < ApplicationRecord
   has_many :games
   validates_presence_of :team1_id, :team2_id
   validates :games_count, presence: true,
-    :numericality => { :greater_than => 0, :less_than => 4}
+    :numericality => { :greater_than => -1, :less_than => 4}
 end
